3c97a894b74832c1fa7e3d53c112ad9411061360
abusaidshabib <abusaidshabib712@gmail.com>
2023-11-17 22:53:22 +0600

    69 done

--- project37/enroll/urls.py
+++ project37/enroll/urls.py
@@ -5,5 +5,5 @@ from .views import home
 register_converter(converters.FourDigitYearConverter, 'yyyy')
 
 urlpatterns = [
-    path('home/<yyyy:my_id>', home,  name="home"),
+    path('home/<yyyy:year>', home,  name="home"),
 ]
